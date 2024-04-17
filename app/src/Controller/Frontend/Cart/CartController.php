102a9348ee0cea24b1a2bb208daca7c1ed2cb29f
filip <filip.penev1@gmail.com>
2024-04-17 10:06:03 +0300

    Created cart controller.

---
+++ app/src/Controller/Frontend/Cart/CartController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Frontend\Cart;
+
+use App\Form\CartType;
+use App\Service\CartStorage;
+use DateTime;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CartController extends AbstractController
+{
+    /**
+     * @throws Exception
+     */
+    public function index(CartStorage $cartStorage, Request $request): Response
+    {
+        $cart = $cartStorage->getCart();
+        $form = $this->createForm(CartType::class, $cart);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cart->setUpdatedAt(new DateTime());
+            $cartStorage->save($cart);
+
+            return $this->redirectToRoute('cart');
+        }
+
+        return $this->render('frontend/cart/cart.html.twig', [
+            'cart' => $cart,
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ app/src/Form/CartType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CartType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('items', CollectionType::class, [
+                'entry_type' => CartItemType::class
+            ])
+            ->add('customerName')
+            ->add('customerEmail')
+            ->add('customerCity')
+            ->add('customerAddress')
+            ->add('paymentMethod')
+            ->add('deliveryMethod')
+            ->add('save', SubmitType::class)
+            ->add('clear', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
